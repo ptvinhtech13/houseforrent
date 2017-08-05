0849250778120ada9669cb7783646f04ba205810
Vinh The Pham <vinhtpham@kms-technology.com>
2017-08-05 21:24:39 +0700

    init project

---
+++ src/main/java/com/ptvinh/houseforrent/houseforrent/HouseforrentApplication.java
@@ -0,0 +1,12 @@
+package com.ptvinh.houseforrent.houseforrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseforrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseforrentApplication.class, args);
+	}
+}
